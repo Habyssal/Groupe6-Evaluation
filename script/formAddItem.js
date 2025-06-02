615d0044104bb9b51a7d881644e3f26349c30ce9
SarCraft <nathan.henaux30@gmail.com>
2025-06-02 12:04:57 +0200

    Refactor product creation logic and update form handling for category addition

---
+++ script/formAddItem.js
@@ -0,0 +1,31 @@
+import { userCreateProduct } from "../modules/products.js";
+
+document.documentElement.style.overflow = 'hidden';
+document.body.style.overflow = 'hidden';
+
+const form = document.getElementById("addItemForm");
+const nameInput = document.querySelector(".name-input");
+const categoryInput = document.querySelector(".category-input");
+const addButton = document.querySelector(".add-product");
+
+const token = "LeMotDePasseEstMotDePasse";
+
+form.addEventListener("input", () => {
+    addButton.disabled = !(nameInput.value && categoryInput.value);
+});
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = nameInput.value;
+    const categorieID = parseInt(categoryInput.value);
+
+    try {
+        const result = await userCreateProduct(token, name, categorieID);
+        alert("Produit ajouté !");
+        form.reset();
+        addButton.disabled = true;
+    } catch (err) {
+        alert("Erreur lors de l'ajout du produit");
+    }
+});
