349fb31d3b486e2c0bcca1cce25802f5622e6fc4
SarCraft <nathan.henaux30@gmail.com>
2025-06-02 12:51:04 +0200

    Fix file paths in HTML and streamline account creation in gestionAccount.js

--- modules/gestionAccount.js
+++ modules/gestionAccount.js
@@ -1,29 +1,30 @@
 const API = "http://localhost:3000" 
 
-
 async function CreateAccount(){
-    const adminaccount = {
-            email: "admin.admin@gmail.com",
-            password: "admin"
-        };
-    
-    res = await fetch (`${API}/signin/`, {
+    res = await fetch (`${API}/signin`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(adminaccount)
+        body: JSON.stringify({ email: "admin.admin@gmail.com", password: "admin" }),
     });
+    if (res.ok) {
+        alert('Compte admin créé !');
+    } else {
+        const data = await res.json();
+        alert(data.error || 'Erreur lors de la création du compte');
+    }
 }
 
+CreateAccount();
 
 async function LoginAccount() {
     const account ={
-        email: document.getElementById('accountEmail').value , //TODO: Ajouter l'Email dans l'Html -> formulaire login
-        password: document.querySelector('#password')          // TODO: Ajouter l'ID dans l'Html -> formulaire login
+        email: document.getElementById('accountEmail').value ,
+        password: document.querySelector('#password').value
     };
 
-    res = await fetch(`${API}/signin/`,{
+    res = await fetch(`${API}/login`,{
         method: 'POST',
         headers: {
             'Content-Type':'application/json'
