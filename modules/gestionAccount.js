defce76a89c23f3a59c5c1904cfdfc0ef6fd737c
SarCraft <nathan.henaux30@gmail.com>
2025-06-02 12:56:05 +0200

    Remove hardcoded admin account details from CreateAccount function

--- modules/gestionAccount.js
+++ modules/gestionAccount.js
@@ -1,11 +1,4 @@
-const API = "http://localhost:3000" 
-
-
 async function CreateAccount(){
-    const adminaccount = {
-            email: "admin.admin@gmail.com",
-            password: "admin"
-        };
     
     res = await fetch (`${API}/signin/`, {
         method: 'POST',
